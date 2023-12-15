621bf7b7f31dadf08a406dc22b1cc15ee8ac746b
Daniel Long <zhozholong@gmail.com>
2023-12-15 15:24:41 -0500

    14, more optimization

--- 14/2-2.cpp
+++ 14/2-2.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-#include <map>
+#include <unordered_map>
 #include <chrono>
 
 using namespace std;
@@ -10,19 +10,28 @@ constexpr int gridsize = 100;
 // << is toward siginifance and also physically left
 // so the left wall is 100000... (1<<99)
 
-void fallWest(vector<__int128_t> &rocks, vector<__int128_t> &blocks) {
-    __int128_t tomove;
-    constexpr __int128_t lwall = (__int128_t)1<<(gridsize-1);
+// for speed, this function (the last in the cycle call) also computes the hash and the weight on the north beams
+pair<__uint128_t,int> fallWest(vector<__uint128_t> &rocks, vector<__uint128_t> &blocks) {
+    __uint128_t hashval = 0;
+    int northweight = 0;
+
+    __uint128_t tomove;
+    constexpr __uint128_t lwall = (__uint128_t)1<<(gridsize-1);
     for (int i = 0; i < gridsize; i++) {
         do {
             tomove = rocks[i] & ~(((blocks[i] | rocks[i]) >> 1) | lwall); // rocks to the right of an empty space
             rocks[i] = (rocks[i] & ~tomove) | (tomove << 1);
         } while (tomove != 0);
+        hashval += rocks[i] ^ (blocks[i]<<3);
+        hashval *= 13;
+        northweight += (gridsize-i)*(__builtin_popcountl(rocks[i]) + __builtin_popcountl(rocks[i]>>64));
     }
+    
+    return make_pair(hashval, northweight);
 }
 
-void fallEast(vector<__int128_t> &rocks, vector<__int128_t> &blocks) {
-    __int128_t tomove;
+void fallEast(vector<__uint128_t> &rocks, vector<__uint128_t> &blocks) {
+    __uint128_t tomove;
     for (int i = 0; i < gridsize; i++) {
         do {
             tomove = rocks[i] & ~(((blocks[i] | rocks[i]) << 1) | 1); // rocks to the left of an empty space
@@ -31,59 +40,58 @@ void fallEast(vector<__int128_t> &rocks, vector<__int128_t> &blocks) {
     }
 }
 
-void fallNorth(vector<__int128_t> &rocks, vector<__int128_t> &blocks) {
+void fallNorth(vector<__uint128_t> &rocks, vector<__uint128_t> &blocks) {
     bool alldone;
+    int starti = 0;
     do {
         alldone = true;
-        for (int i = 0; i < (gridsize-1); i++) {
-            __int128_t tomove = rocks[i+1] & (~(rocks[i] | blocks[i])); // rocks in the lower row where I don't have a rock
+        for (int i = starti; i < (gridsize-1); i++) {
+            __uint128_t tomove = rocks[i+1] & (~(rocks[i] | blocks[i])); // rocks in the lower row where I don't have a rock
             if (tomove != 0) {
                 rocks[i+1] ^= tomove;
                 rocks[i] |= tomove;
                 alldone = false;
+            } else if (alldone) {
+                starti++;
             }
         }
+        starti = max(0,starti-1);
     } while(!alldone);
 }
 
-void fallSouth(vector<__int128_t> &rocks, vector<__int128_t> &blocks) {
+void fallSouth(vector<__uint128_t> &rocks, vector<__uint128_t> &blocks) {
     bool alldone;
+    int starti = (gridsize-1);
     do {
         alldone = true;
         for (int i = (gridsize-1); i > 0; i--) {
-            __int128_t tomove = rocks[i-1] & (~(rocks[i] | blocks[i])); // rocks in the higher row where I don't have a rock
+            __uint128_t tomove = rocks[i-1] & (~(rocks[i] | blocks[i])); // rocks in the higher row where I don't have a rock
             if (tomove != 0) {
                 rocks[i-1] ^= tomove;
                 rocks[i] |= tomove;
                 alldone = false;
+            } else if (alldone) {
+                starti--;
             }
         }
+        min((gridsize-1),starti+1);
     } while(!alldone);
 }
 
 
-void cycle(vector<__int128_t> &rocks, vector<__int128_t> &blocks) {
+pair<__uint128_t,int> cycle(vector<__uint128_t> &rocks, vector<__uint128_t> &blocks) {
     fallNorth(rocks,blocks);
     fallEast(rocks,blocks);
     fallSouth(rocks,blocks);
-    fallWest(rocks,blocks);
+    return fallWest(rocks,blocks);
 }
 
-__int128_t ghash(vector<__int128_t> &rocks, vector<__int128_t> &blocks) {
-    __int128_t h = 0;
-    for (int i = 0; i < rocks.size(); i++) {
-        h += rocks[i] ^ (blocks[i]<<3);
-        h *= 17;
-    }
-    return h;
-}
-
-void pprint(vector<__int128_t> &rocks, vector<__int128_t> &blocks) {
+void pprint(vector<__uint128_t> &rocks, vector<__uint128_t> &blocks) {
     for (int i = 0; i < gridsize; i++) {
         for (int j = 0; j < gridsize; j++) {
-            if (rocks[i] & ((__int128_t)1<<j)) {
+            if (rocks[i] & ((__uint128_t)1<<j)) {
                 cout << 'O';
-            } else if (blocks[i] & ((__int128_t)1<<j)) {
+            } else if (blocks[i] & ((__uint128_t)1<<j)) {
                 cout << '#';
             } else {
                 cout << '.';
@@ -95,12 +103,13 @@ void pprint(vector<__int128_t> &rocks, vector<__int128_t> &blocks) {
 
 int main() {
     auto start_time = chrono::high_resolution_clock::now();
+    ios_base::sync_with_stdio(false);
     string line;
-    vector<__int128_t> rocks;
-    vector<__int128_t> blocks;
+    vector<__uint128_t> rocks;
+    vector<__uint128_t> blocks;
     while (getline(cin, line)) {
-        __int128_t r = 0;
-        __int128_t b = 0;
+        __uint128_t r = 0;
+        __uint128_t b = 0;
         for (int i = line.size()-1; i >= 0; i--) {
             if (line[i] == 'O') {
                 r++;
@@ -116,26 +125,22 @@ int main() {
         blocks.push_back(b);
     }
 
-    map<__int128_t,int> hist;
+    unordered_map<__uint128_t,int> hist;
+    unordered_map<int,int> weights;
     int iter = 0;
-    while (hist.find(ghash(rocks,blocks)) == hist.end()) {
-        hist[ghash(rocks,blocks)] = iter;
-        cycle(rocks,blocks);
+    __uint128_t lasthash = 0;
+    while (hist.find(lasthash) == hist.end()) {
+        auto [h,w] = cycle(rocks,blocks);
+        hist[lasthash] = iter;
+        lasthash = h;
+        weights[iter] = w;
         iter++;
     }
-    int cycstart = hist[ghash(rocks,blocks)];
+    int cycstart = hist[lasthash];
     int cyclen = iter - cycstart;
     int leftover = (1000000000 - cycstart)%cyclen;
-    for (int _ = 0; _ < leftover; _++) cycle(rocks,blocks);
-    int ans = 0;
-    for (int i = 0; i < 100; i++) {
-        for (int j = 0; j < 100; j++) {
-            if ((rocks[i] & ((__int128_t)1<<j)) > 0) {
-                ans += gridsize-i;
-            }
-        }
-    }
-    cout << ans << endl;
+    
+    cout << weights[cycstart+leftover-1] << endl;
 
     auto end_time = chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time);
