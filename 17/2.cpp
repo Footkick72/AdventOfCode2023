5ee82dcf8e8540a9615a422caab5efa253afd34e
Daniel Long <zhozholong@gmail.com>
2023-12-17 15:29:15 -0500

    17 - added djikstra precomute to improve A* heuristic

--- 17/2.cpp
+++ 17/2.cpp
@@ -25,13 +25,20 @@ struct State {
 
     State(int i, int j, int s, int d) : i(i), j(j), straight(s), dir(d) {}
 
-    vector<State> opts() const {
-        vector<State> o;
+    vector<pair<State,int>> opts(vector<vector<int>> &grid) const {
+        vector<pair<State,int>> o;
         for (int d : {(this->dir+3)%4, this->dir, (this->dir+1)%4}) {
             auto [di,dj] = dirs[d];
-            if (i + di >= 0 && i + di < maxi && j + dj >= 0 && j + dj < maxj) {
-                if ((d == this->dir && straight < 10) || (d != this->dir && straight >= 4)) {
-                    o.emplace_back(i+di, j+dj, (d == this->dir) ? this->straight+1 : 1, d);
+            if (d != this->dir && straight >= 4) {
+                if (i + 4*di >= 0 && i + 4*di < maxi && j + 4*dj >= 0 && j + 4*dj < maxj) {
+                    int c = 0;
+                    for (int k = 1; k <= 4; k++) c += grid[i+k*di][j+k*dj];
+                    o.emplace_back(State{i+4*di, j+4*dj, 4, d}, c);
+                }
+            }
+            if (d == this->dir && straight < 10) {
+                if (i + di >= 0 && i + di < maxi && j + dj >= 0 && j + dj < maxj) {
+                    o.emplace_back(State{i+di, j+dj, this->straight+1, d}, grid[i+di][j+dj]);
                 }
             }
         }
@@ -49,20 +56,22 @@ struct StateHash {
     }
 };
 
-struct Hueristic {
+struct Heuristic {
     const unordered_map<State,int,StateHash> &dists;
+    const vector<vector<int>> &heuristic;
 
-    Hueristic(const unordered_map<State,int,StateHash> &d) : dists(d) {}
+    Heuristic(const unordered_map<State,int,StateHash> &d, const vector<vector<int>> &h) : dists(d), heuristic(h) {}
 
     bool operator()(const State &a, const State &b) const {
-        return dists.at(a) + (maxi - a.i) + (maxj - a.j) > dists.at(b) + (maxi - b.i) + (maxj - b.j);
+        // return dists.at(a) + (maxi - a.i) + (maxj - a.j) > dists.at(b) + (maxi - b.i) + (maxj - b.j);
+        return dists.at(a) + heuristic[a.i][a.j] > dists.at(b) + heuristic[b.i][b.j];
     }
 };
 
-int shortestPath(State start, vector<vector<int>> &grid) {
+int shortestPath(State start, vector<vector<int>> &grid, vector<vector<int>> &heuristic) {
     unordered_map<State,int,StateHash> dists;
-    Hueristic h(dists);
-    priority_queue<State, vector<State>, Hueristic> open(h);
+    Heuristic h(dists, heuristic);
+    priority_queue<State, vector<State>, Heuristic> open(h);
 
     State s1(start.i, start.j, start.straight, 0);
     State s2(start.i, start.j, start.straight, 1);
@@ -75,20 +84,54 @@ int shortestPath(State start, vector<vector<int>> &grid) {
         auto current = open.top();
 
         if (current.i == maxi-1 && current.j == maxj-1 && current.straight >= 4) {
-            int d = dists[current];
-            return d;
+            return dists[current];
         }
         open.pop();
 
-        for (auto opt : current.opts()) {
-            int tdist = dists[current] + grid[opt.i][opt.j];
-            if (dists.find(opt) == dists.end() || tdist < dists.at(opt)) {
-                dists[opt] = tdist;
-                open.push(opt);
+        for (auto opt : current.opts(grid)) {
+            int tdist = dists[current] + opt.second;
+            if (dists.find(opt.first) == dists.end() || tdist < dists.at(opt.first)) {
+                dists[opt.first] = tdist;
+                open.push(opt.first);
             }
         }
     }
     return -1;
+}
+
+vector<vector<int>> revDijkstra(vector<vector<int>> &grid) {
+    vector<vector<int>> dists;
+    for (int i = 0; i < maxi; i++) {
+        dists.emplace_back();
+        for (int j = 0; j < maxj; j++) {
+            dists.back().emplace_back(INT_MAX);
+        }
+    }
+    auto g = [](const pair<int,int> &p1, const pair<int,int> &p2) {
+        return p1 > p2;
+    };
+    priority_queue<pair<int,int>, vector<pair<int,int>>, decltype(g)> open(g);
+
+    open.push({maxi-1, maxj-1});
+    dists[maxi-1][maxj-1] = 0;
+
+    while (!open.empty()) {
+        auto current = open.top();
+        open.pop();
+
+        for (auto [di,dj] : array<array<int,2>,4>{{{0,1},{0,-1},{1,0},{-1,0}}}) {
+            if (current.first + di >= 0 && current.first + di < maxi && current.second + dj >= 0 && current.second + dj < maxj) {
+                int ni = current.first+di;
+                int nj = current.second+dj;
+                int tdist = dists[current.first][current.second] + grid[ni][nj];
+                if (tdist < dists[ni][nj]) {
+                    dists[ni][nj] = tdist;
+                    open.push({ni,nj});
+                }
+            }
+        }
+    }
+    return dists;
 }
 
 int main() {
@@ -102,5 +145,15 @@ int main() {
     }
     maxi = grid.size();
     maxj = grid[0].size();
-    cout << shortestPath({0,0,0,0},grid) << endl;
+    auto start_time = chrono::high_resolution_clock::now();
+    auto heuristic = revDijkstra(grid);
+    auto end_time = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<std::chrono::microseconds>(end_time - start_time);
+    int djikstratime = duration.count();
+    start_time = chrono::high_resolution_clock::now();
+    cout << shortestPath({0,0,0,0},grid,heuristic) << endl;
+    end_time = chrono::high_resolution_clock::now();
+    duration = chrono::duration_cast<std::chrono::microseconds>(end_time - start_time);
+    cout << djikstratime/1000.0 << " ms Djikstra Precompute" << endl;
+    cout << duration.count()/1000.0 << " ms A*" << endl;
 }
